1f9fa9a0d8108194e84e1281b2329c23afd325b5
Olaf Hartig <olaf.hartig@liu.se>
2018-09-05 20:02:21 +0200

    added subdirectories for client-specific scripts and coordination scripts

---
+++ MultiClientExperiments/ClientScripts/pack_results.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+EXEC_DIR_ON_NODE=/home/admin/tpf_evaluation/brTPF_Experiments/MultiClientExperiments/ClientScripts
+ZIP_DIR_ON_NODE=${EXEC_DIR_ON_NODE}/output
+ZIP_FILE_ON_NODE=${ZIP_DIR_ON_NODE}/${3}clients.${1}.${2}.csv.zip
+
+mkdir -p ${ZIP_DIR_ON_NODE}
+zip ${ZIP_FILE_ON_NODE} ${EXEC_DIR_ON_NODE}/eval*.csv ${EXEC_DIR_ON_NODE}/executed_queries_list_*
+
+rm ${EXEC_DIR_ON_NODE}/eval*.csv
+rm ${EXEC_DIR_ON_NODE}/executed_queries_list_*
--- MultiClientExperiments/pack_results.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-mkdir -p output
-zip /home/admin/tpf_evaluation/Client.js/output/${3}clients.${1}.${2}.csv.zip /home/admin/tpf_evaluation/Client.js/eval*.csv /home/admin/tpf_evaluation/Client.js/executed_queries_list_*
-rm /home/admin/tpf_evaluation/Client.js/eval*.csv /home/admin/tpf_evaluation/Client.js/executed_queries_list_*
