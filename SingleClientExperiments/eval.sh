60cf9763d90ad6605d108d811961438fe6513ffc
Olaf Hartig <olaf.hartig@liu.se>
2018-09-06 20:33:57 +0200

    extended eval.sh to take more parameters

--- SingleClientExperiments/eval.sh
+++ SingleClientExperiments/eval.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
-for query in $3/*.rq
+# Author: Olaf Hartig  http://olafhartig.de
+
+if [ "$#" -lt 5 ]; then
+   echo "Illegal number of parameters."
+	echo
+	echo "Usage: ./eval.sh <dir with queries> <URI of start fragment> <timeout in minutes> (TPF-client-eval | brTPF-client-eval) <string to be attached to file name> <max. number of mappings (only for brTPF)>"
+	echo
+	echo "Example: ./eval.sh ../Queries/WatDiv/OneDir100  http://localhost:8080/watdiv  60  brTPF-client-eval  test  30"
+	echo
+	exit
+fi
+
+for query in $1/*.rq
 do
 	echo $query
-	echo "$1 -c $2 $query"
-	results=$(timeout 15m ../ExtendedClient.js/bin/$1 -c $2 $query --maxNumberOfMappings 30)
+	results=$(timeout 15m ../ExtendedClient.js/bin/$4 $2 $query --timeoutInMins $3 --outputFileNumber $5 --maxNumberOfMappings $6)
 	echo "$results"
 done
