003db1e975d5948e79d1165bbf8cdda088e6d067
Olaf Hartig <olaf.hartig@liu.se>
2018-09-07 16:48:46 +0200

    added new parameter --timeoutInMins to eval_parallel.py

--- MultiClientExperiments/ClientScripts/eval_parallel.py
+++ MultiClientExperiments/ClientScripts/eval_parallel.py
@@ -6,13 +6,13 @@ import subprocess
 from multiprocessing import Pool
 
 
-def main_parallel(command, start_fragment, config_file, query_folders, batch, cores):
+def main_parallel(command, start_fragment, config_file, query_folders, batch, cores, timeoutInMins):
     arglist = []
     AVAILABLE_CORES = int(cores)
     folder_number = 0
     for query_folder in sorted(glob.glob(query_folders + '/*')):
         print(query_folder)
-        arglist.append((command, start_fragment, config_file, query_folder, batch, folder_number))
+        arglist.append((command, start_fragment, config_file, query_folder, batch, timeoutInMins, folder_number))
         folder_number += 1
 
     print('Processing ' + str(folder_number) + ' files with ' +
@@ -21,13 +21,13 @@ def main_parallel(command, start_fragment, config_file, query_folders, batch, co
     pool.map_async(main, arglist).get()
 
 
-def main((command, start_fragment, config_file, query_folder, batch, folder_number)):
+def main((command, start_fragment, config_file, query_folder, batch, timeoutInMins, folder_number)):
     for query_file in sorted(glob.glob(query_folder + '/*.rq')):
         print('Query: ' + query_file)
         query_list_file_name = 'executed_queries_list_' +  command + '_' + str(folder_number) + '.txt'
         with open(query_list_file_name, 'a') as query_list_file:
             query_list_file.write(query_file + '\n')
-            cmd = '../../ExtendedClient.js/bin/' + command + ' ' + start_fragment + ' -c ' + config_file + ' -f ' + os.path.join(os.path.dirname(os.path.realpath(__file__)), query_file + ' --maxNumberOfMappings ' + batch  + ' --outputFileNumber ' + str(folder_number))
+            cmd = '../../ExtendedClient.js/bin/' + command + ' ' + start_fragment + ' -c ' + config_file + ' -f ' + os.path.join(os.path.dirname(os.path.realpath(__file__)), query_file + ' --maxNumberOfMappings ' + batch  + ' --timeoutInMins ' + timeoutInMins + ' --outputFileNumber ' + str(folder_number))
             print('Command: ' + cmd)
             try:
                 subprocess.call(cmd, shell=True)
@@ -41,7 +41,7 @@ if __name__ == '__main__':
         print('Usage: python eval.py config_file query_folder')
     else:
         try:
-            main_parallel(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5], sys.argv[6])
+            main_parallel(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5], sys.argv[6], sys.argv[7])
         except Exception, exc:
             print exc
             sys.exit()
