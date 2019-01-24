b9a7647cb96732533cac4017719f007da7684a64
Carlos Buil <cbuil@inf.utfsm.cl>
2019-01-24 16:57:30 -0300

    modified packing reuslts script

--- MultiClientExperiments/ClientScripts/pack_results.sh
+++ MultiClientExperiments/ClientScripts/pack_results.sh
@@ -5,7 +5,8 @@ ZIP_DIR_ON_NODE=${EXEC_DIR_ON_NODE}/output
 ZIP_FILE_ON_NODE=${ZIP_DIR_ON_NODE}/${3}clients.${1}.${2}.csv.zip
 
 mkdir -p ${ZIP_DIR_ON_NODE}
-zip ${ZIP_FILE_ON_NODE} ${EXEC_DIR_ON_NODE}/eval*.csv ${EXEC_DIR_ON_NODE}/executed_queries_list_*
+zip ${ZIP_FILE_ON_NODE} ${EXEC_DIR_ON_NODE}/eval*.csv ${EXEC_DIR_ON_NODE}/executed_queries_list_* ${EXEC_DIR_ON_NODE}/sage_output/*.csv
 
 rm ${EXEC_DIR_ON_NODE}/eval*.csv
+rm ${EXEC_DIR_ON_NODE}/sage_output/*.csv
 rm ${EXEC_DIR_ON_NODE}/executed_queries_list_*
